912969c42aa763bd74c20d6e378ad330282c2c37
Maks Tish <decim.meri@gmail.com>
2025-11-25 02:43:22 +0300

    end render button nav

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,15 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY'; //храним ключи, В дальнейшем на TypeScripte это понадобиться 
-// utils
 
+//page
+const page = {                  //Описывает работу Меню
+    menu: document.querySelector('.menu__list')
+}
+
+
+
+// utils
 //Функция получения данных от пользователя 
 function loadData() {                   //localStorage.getItem() — это метод в JavaScript, используемый для получения значения, сохраненного в localStorage
     const habbitsString = localStorage.getItem('HABBIT_KEY'); //Если у нас есть ключи их лучше хранить в отдел. константе
@@ -20,10 +27,53 @@ function saveData() {
     localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits)); //localStorage.setItem() — это метод в JavaScript, который используется для сохранения данных в локальном хранилище браузера.
 
 }
+
+//render
+function rerenderMenu(activeHabbit) {
+    if (!activeHabbit){           //Проверка если у нас нет хеббитАйди
+        return;
+    }
+    //Если у нас есть значение хеббитАйди, то мы идем по массиву
+    for (const habbit of habbits) {
+       const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`); //Задаём индетифик в хтмл файле и сюда привязываем к квериСелектору
+        if (!existed) {  //Если нет, то создаем
+            //Создание
+            const element = document.createElement('button');        //Создаём кнопку
+            element.setAttribute('menu-habbit-id', habbit.id);      //Добавляем атрибут в дальнейшем для поиска. Чтобы потом при следующем ререндере его найти
+            element.classList.add('menu__item');
+            element.addEventListener('click', () => rerender(habbit.id))     //Добавляем обработчки событий, в дальнейшем для вызова ререндера
+            element.innerHTML = `<img src="./images/${habbit.icon}.svg" alt="${habbit.name}"/>`;//Сюда кладём наш ИМГ
+            if (activeHabbit.id === habbit.id) {                         
+                element.classList.add('menu__item_active');
+            }
+            page.menu.appendChild(element);                                                //Теперь при обращении к меню и добавляем дочерний элемент
+            continue;                                               //Добавили, так как у нас нарушена логика, и чтобы мы перешли к след. элемент. цикла
+        }
+            if (activeHabbit.id === habbit.id) {                         //Привычки которые мы проходимся с помощью классЛиста, добавляем название актив эл-лт.
+            existed.classList.add('menu__item_active');
+        } else {
+            existed.classList.remove('menu__item_active');          //Если нет, не активный, удаляем
+        }
+    }
+}
+//Рендер шапки
+function renderHead(activeHabbit){
+
+}
+
+function rerender(activeHabbitId) {
+    const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);  //
+    rerenderMenu(activeHabbit);
+    renderHead(activeHabbit);
+}
+
+
+//init
 //Для того чтобы инициализация была один раз будем использовать Intermediate local function в виде стрелоч. функции
 // и в рамках этой функции будем исполнять loadData()
 (() => {
     loadData();
-})()
+    rerenderMenu(habbits[0].id);
+})();
 
 //Добавили минимально данных для динам. проверки, потому что без данных это будет трудно сделать
